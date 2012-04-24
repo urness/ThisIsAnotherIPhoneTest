1d836dcac83cbaabaaf260d7dc095892e7ee4338
Timothy Urness <timothy.urness@drake.edu>
2012-04-24 15:52:49 -0500

    First Committ

---
+++ YAT/YAT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YAT
+//
+//  Created by Timothy Urness on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YAT/YAT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YAT
+//
+//  Created by Timothy Urness on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
